f7006da16477c4c26ea666a7cbfda3f172c239df
Andrzej Bieniek <andyhelp@gmail.com>
2010-08-27 22:47:56 +0100

    Added url and view that sets the userskin then redirects to the same url.

--- userskins/views.py
+++ userskins/views.py
@@ -1,1 +1,27 @@
 # Create your views here.
+from django import http
+from django.conf import settings
+
+def set(request):
+    """
+    Redirect to a given url while setting the chosen language in the
+    session or cookie. The url and the language code need to be
+    specified in the request parameters.
+
+    Since this view changes how the user will see the rest of the site, it must
+    only be accessed as a POST request. If called as a GET request, it will
+    redirect to the page in the request (the 'next' parameter) without changing
+    any state.
+    """
+    next = request.REQUEST.get('next', None)
+    if not next:
+        next = request.META.get('HTTP_REFERER', None)
+    if not next:
+        next = '/'
+    response = http.HttpResponseRedirect(next)
+    if request.method == 'POST':
+        skin = request.POST.get('skin', None)
+        if skin and skin in settings.USERSKINS_DETAILS:
+            response.set_cookie("userskins", str(skin))
+            
+    return response
