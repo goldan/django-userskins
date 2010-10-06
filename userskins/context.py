6827ddad80c3f999246a260737d0901d04bf7b2b
Denis Golomazov <denis.golomazov@gmail.com>
2010-10-06 08:15:44 +0400

    Added userskins_skin_list variable to context in context processor.

--- userskins/context.py
+++ userskins/context.py
@@ -6,8 +6,9 @@ def userskins(request):
     skin = settings.USERSKINS_DEFAULT
     if request.COOKIES.has_key("userskins") and request.COOKIES["userskins"] in settings.USERSKINS_DETAILS:
         skin = request.COOKIES["userskins"]
+    skin_list = settings.USERSKINS_DETAILS.keys()
     if getattr(settings,"USERSKINS_USE_COMPRESS_GROUPS",False):
-        return {"userskins_skin": skin, "userskins_use_compress":True }
+        return {"userskins_skin": skin, "userskins_use_compress":True, "userskins_skin_list": skin_list }
     else:
         skin_uri = u"%s%s" % (settings.STATIC_URL, settings.USERSKINS_DETAILS[skin])
-        return {"userskins_current_skin_name": skin, "userskins_skin": skin_uri, "userskins_use_compress":False }
+        return {"userskins_current_skin_name": skin, "userskins_skin": skin_uri, "userskins_use_compress":False, "userskins_skin_list": skin_list }
